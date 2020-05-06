70c767fc6dfdffa55c9d3808bd250a2b806d731d
Christopher Osborn <z68ozz@gmail.com>
2020-05-06 00:47:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,10 @@ end
 end
 
 def age_appropriate_birthday(birthday_kids)
-  
+ 
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
+end
 
 
